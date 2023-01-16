d82aeb8cde1575983c55af331a0d7f73cdfb6fb8
a név minek <usscommando@gmail.com>
2023-01-16 12:17:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let videoNewID;
+$('#loadVideoById').on('change', function(){
+
+    var newval = '',
+        $this = $(this);
+
+    if (newval = $this.val().match(/(\?|&)v=([^&#]+)/)) {
+
+        videoNewID = $this.val(newval.pop());
+
+    } else if (newval = $this.val().match(/(\.be\/)+([^\/]+)/)) {
+
+        videoNewID = $this.val(newval.pop());
+
+    } else if (newval = $this.val().match(/(\embed\/)+([^\/]+)/)) {
+
+        $this.val(newval.pop().replace('?rel=0',''));
+        videoNewID = $this.val(newval.pop().replace('?rel=0',''));
+    }
+
+});
+
