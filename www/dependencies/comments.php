9b47657a6c8ca346474f5232770ff786daeba46e
DrSzklenar <usscommando@gmail.com>
2023-03-29 13:26:29 +0200

    code optimalisation

--- www/dependencies/comments.php
+++ www/dependencies/comments.php
@@ -17,42 +17,15 @@ $commentField = "<div class=\"commentSection\">
     <div class=\"commentContainer littleGap\">";
 
 
-if (isset($_GET['gameid'])) {
-    $whereAmI = "game";
-    $wholeGrainId = $id;
-    $commentsSQL = "SELECT 
-    comment.id AS 'comment_id' ,comment.parentID,comment.madeBy,comment.madeOn,comment.type,comment.date,comment.text, user.id AS 'user_id' , user.nev, user.avatar, IFNULL(SUM(ratios.ratio),0) as ratio
-    FROM `comment` LEFT JOIN `user` ON comment.madeBy = user.id 
-    LEFT JOIN `ratios` ON ratios.commentID = comment.id 
-    WHERE comment.type = '{$whereAmI}' AND comment.madeOn = '{$wholeGrainId}'GROUP BY comment.id  ORDER BY comment.date DESC;";
-    $queriedComments = mysqli_query($conn, $commentsSQL);
-    while ($commentRow = mysqli_fetch_array($queriedComments)) {
-        $commentField .= "<div id=\"{$commentRow['comment_id']}\" class=\"comment flexrow littleGap\">
-            <div class=\"leftside\">
-                <div class=\"rating flexrow littleGap\">
-                    <div class=\"ratingValue\">{$commentRow['ratio']}</div>
-                    <div class=\"ratingButtons flexcol\">
-                        <div class=\"upbutton\"><img src=\"https://media.istockphoto.com/id/157030584/vector/thumb-up-emoticon.jpg?s=1024x1024&w=is&k=20&c=PtntOtwm-DY9ETqOo23RQKUS48_ZfYUkKpcZXWzEfNI=\" alt=\"\"></div>
-                        <div class=\"downbutton\"><img src=\"https://play-lh.googleusercontent.com/G4PFUhWRDby0PDKCzNQU8H6uCngprpDGfz_LSDpKdCXVlAj5qM-Kq6TAvlgWemtbnlA\" alt=\"\"></div>
-                    </div>
-                </div>
-            </div>
-            <div class=\"rightside flexcol\">
-                <div class=\"nameAndDate flexrow littleGap\">
-                    <img class=\"commentAvatar\" src=\"{$commentRow['avatar']}\" alt=\"\">
-                    <h2><a href=\"profile.php?user=".$commentRow['nev']."&userid=".$commentRow['user_id']."\"class=\"\">{$commentRow['nev']}</a></h2>
-                    <h4>{$commentRow['date']}</h4>
-                </div>
-                <div class=\"content\">
-                    <p>{$commentRow['text']}</p>
-                </div>
-            </div>
-        </div>";
+if (isset($_GET['gameid']) || isset($_GET['userid'])) {
+    if (isset($_GET['gameid'])) {
+        $whereAmI = "game";
+        $wholeGrainId = $id;
     }
-
-} else if (isset($_GET['userid'])) {
-    $wholeGrainId = $userid;
-    $whereAmI = "user";
+    else if (isset($_GET['userid'])) {
+        $wholeGrainId = $userid;
+        $whereAmI = "user";
+    }
     $commentsSQL = "SELECT 
     comment.id AS 'comment_id' ,comment.parentID,comment.madeBy,comment.madeOn,comment.type,comment.date,comment.text, user.id AS 'user_id' , user.nev, user.avatar, IFNULL(SUM(ratios.ratio),0) as ratio
     FROM `comment` LEFT JOIN `user` ON comment.madeBy = user.id 
@@ -106,16 +79,10 @@ if (isset($_GET['gameid'])) {
                 $commentField .= "<div class=\"kuka\"><i class=\"fa fa-trash\"></i></div>";
                 
             }
-            
-
-
             $commentField .="
         </div>";
     } 
-
-
 }
-
 
 $commentField .= "</div>
 
@@ -151,9 +118,6 @@ let {$commentInput} = document.getElementById(\"{$commentInput}\");
         })
         .catch(error => console.log(error));
         console.log(\"gag\");
-        
-
-        
     }
     });
 
@@ -255,58 +219,5 @@ let {$commentInput} = document.getElementById(\"{$commentInput}\");
 
         
 </script>";
-
-
 echo $commentField;
-
-
 ?>
-<!-- <script type="text/javascript">
-    let upbuttons = document.querySelectorAll(".upbutton");
-    let downbuttons = document.querySelectorAll(".downbutton");
-
-    function PushVotes(element,stat=""){
-        let parentID = element.parentElement.parentElement.parentElement.parentElement.id;
-        if (stat = "plus") {
-            element.parentElement.parentElement.firstChild.innerHTML += 1;
-            element.parentElement.parentElement.firstChild.nextSibling.innerHTML = parseInt(element.parentElement.parentElement.firstChild.nextSibling.innerHTML) +1
-        }
-        else if (stat = "minus") {
-            
-        }
-        
-        
-    }
-    for (let i = 0; i < upbuttons.length; i++) {
-        upbuttons[i].addEventListener('click',() => PushVotes(upbuttons[i],"plus"));
-        downbuttons[i].addEventListener('click',() => PushVotes(upbuttons[i],"minus"));
-    }  
-
-
-    function DeleteComment(element) {
-        let dataForPHP = new FormData();
-        dataForPHP.append("madeOn", element.parentElement.id);
-        dataForPHP.append("type", "delete");
-        fetch(`dependencies/pushComment.php`, {
-            method: "POST",
-            body: dataForPHP
-        })
-        .then(response => response.text())
-        .then(data => {
-            console.log(data)
-            if (data == "DELETED") {
-                window.location.reload();
-            }
-        })
-        .catch(error => console.log(error));
-        console.log("gag");
-    }
-
-    let kukas = document.querySelectorAll(".kuka");
-    for (let i = 0; i < kukas.length; i++) {
-        kukas[i].addEventListener('click',() => DeleteComment(kukas[i]));
-    }
-
-    
-</script> -->
-
